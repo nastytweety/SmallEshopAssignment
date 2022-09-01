e85d457aaef7166532aa4ee08af7f508f046daf9
ndiakos <ndiakos@minedu.gov.gr>
2022-09-01 11:13:53 +0300

    - Db Added - Services Added - Model Added

---
+++ SmallEshopAssignment/Model/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SmallEshopAssignment.Model
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            Seed(builder);
+        }
+
+        private void Seed(ModelBuilder builder)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+       // public DbSet<Ticker> Tickers { get; set; }
+       // public DbSet<Currency> Currencies { get; set; }
+    }
+}
--- SmallEshopAssignment/Program.cs
+++ SmallEshopAssignment/Program.cs
@@ -1,6 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using SmallEshopAssignment.Model;
+
 var builder = WebApplication.CreateBuilder(args);
+ConfigurationManager configuration = builder.Configuration;
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("ConnStr")));
+//builder.Services.AddScoped<IApplicationService, ApplicationService>();
+//builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+//builder.Services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
+builder.Services.AddControllers().AddNewtonsoftJson(options =>
+{
+    options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
